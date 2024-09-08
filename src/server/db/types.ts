bed753834368b264d39cfb5855786e6be3816a12
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-09-08 20:31:11 +1000

    db types

---
+++ src/server/db/types.ts
@@ -0,0 +1,27 @@
+export interface Match {
+  id: number;
+  player1_id: number;
+  player2_id: number;
+  location?: string;
+  matchtime: Date;
+  player1_score: number;
+  player2_score: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Player {
+  id: number;
+  first_name?: string;
+  last_name?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  team_id: number;
+}
+
+export interface Team {
+  id: number;
+  team_name?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
